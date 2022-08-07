aa3317085a6f31f702527e056f7e8811d1e20424
marianobrc <marianomartinez84@gmail.com>
2022-08-07 01:04:06 -0300

    Bugfixes in modals

--- js/mymodal.js
+++ js/mymodal.js
@@ -1,26 +1,24 @@
-const images = document.querySelectorAll(".item img");
-    // get images src onclick
-    images.forEach((img) => {
+document.querySelectorAll(".item img").forEach((img) => {
         img.addEventListener("click", (e) => {
             //run modal function
             imgModal(e.target.src);
         });
     });
-    //creating the modal
-    function imgModal(src){
-        const modal = document.createElement("div");
-        modal.setAttribute("class", "modal");
-        //add the modal to the main section or the root element of the page
-        document.querySelector(".ajax-page-content").append(modal);
-        //adding image to modal
-        const newImage = document.createElement("img");
-        newImage.setAttribute("src", src);
-        //creating the close button
-        const closeBtn = document.createElement("i");
-        closeBtn.setAttribute("class", "fas fa-times closeBtn");
-        //close function
-        closeBtn.onclick = () => {
-            modal.remove();
-        };
-        modal.append(newImage, closeBtn);
+//creating the modal
+function imgModal(src){
+    const modal = document.createElement("div");
+    modal.setAttribute("class", "modal");
+    //add the modal to the main section or the root element of the page
+    document.querySelector(".ajax-page-content").append(modal);
+    //adding image to modal
+    const newImage = document.createElement("img");
+    newImage.setAttribute("src", src);
+    //creating the close button
+    const closeBtn = document.createElement("i");
+    closeBtn.setAttribute("class", "fas fa-times closeBtn");
+    //close function
+    closeBtn.onclick = () => {
+        modal.remove();
     };
+    modal.append(newImage, closeBtn);
+};
