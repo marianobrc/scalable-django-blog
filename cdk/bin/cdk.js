a13ab5a806d0dea0d1ab0ccbf0c46c2483cfa3b3
marianobrc <marianomartinez84@gmail.com>
2022-08-08 15:32:16 -0300

    Complete the infrastructure code to deploy it in S3 with CDK

--- cdk/bin/cdk.js
+++ cdk/bin/cdk.js
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 
 const cdk = require('aws-cdk-lib');
-const { CdkStack } = require('../lib/cdk-stack');
+const { PersonalWebsiteStack } = require('../lib/cdk-stack');
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new PersonalWebsiteStack(app, 'PersonalWebsiteStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  certificateArn: 'arn:aws:acm:us-east-1:675985711616:certificate/f3f30b76-b7d3-496c-aab0-2f46428cd077',
+  domainNames: ['marianomartinezgrasso.com', 'www.marianomartinezgrasso.com']
 });
--- cdk/lib/cdk-stack.js
+++ cdk/lib/cdk-stack.js
@@ -1,23 +1,77 @@
-const { Stack, Duration } = require('aws-cdk-lib');
-// const sqs = require('aws-cdk-lib/aws-sqs');
+const { Stack } = require('aws-cdk-lib');
+const cdk = require('aws-cdk-lib');
+const s3 = require('aws-cdk-lib/aws-s3')
+const s3Deploy = require('aws-cdk-lib/aws-s3-deployment')
+const cloudfront = require('aws-cdk-lib/aws-cloudfront')
+const certManager = require('aws-cdk-lib/aws-certificatemanager');
 
-class CdkStack extends Stack {
-  /**
-   *
-   * @param {Construct} scope
-   * @param {string} id
-   * @param {StackProps=} props
-   */
+
+class PersonalWebsiteStack extends Stack {
   constructor(scope, id, props) {
-    super(scope, id, props);
+    super(scope, id, props)
+    const { certificateArn, domainNames } = props
+    const certificate = certManager.Certificate.fromCertificateArn(this, `SSLCertificate`, certificateArn);
+    // S3 bucket to store teh static website
+    const bucketID = `PersonalWebsiteBucket`
+    this.bucket = new s3.Bucket(this, bucketID, {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+    })
 
-    // The code that defines your stack goes here
+    // Create an Origin Access Identity to allow CloudFront access this bucket
+    this.oia = new cloudfront.OriginAccessIdentity(this, `${bucketID}OIA`, {
+      comment: 'OIA created by CDK',
+    })
+    this.bucket.grantRead(this.oia)
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: Duration.seconds(300)
-    // });
+    // Deployment
+    // eslint-disable-next-line no-new
+    new s3Deploy.BucketDeployment(this, `DeployPersonalWebsite`, {
+      sources: [s3Deploy.Source.asset('../', {
+        exclude: [
+          'node_modules',
+          '.git',
+          'cdk.out'
+        ]
+      })],
+      destinationBucket: this.bucket,
+    })
+
+    // Cloudfront
+    this.cloudfrontDistro = new cloudfront.CloudFrontWebDistribution(
+      this,
+      `PersonalWebsiteCloudfrontDistribution`,
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: this.bucket,
+              originAccessIdentity: this.oia,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+        errorConfigurations: [
+          {
+            errorCode: 403,
+            responseCode: 200,
+            responsePagePath: '/index.html',
+          },
+          {
+            errorCode: 404,
+            responseCode: 200,
+            responsePagePath: '/index.html',
+          },
+        ],
+        viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(
+          certificate,{
+            aliases: domainNames
+          })
+      },
+    )
   }
 }
 
-module.exports = { CdkStack }
+module.exports = { PersonalWebsiteStack }
+
